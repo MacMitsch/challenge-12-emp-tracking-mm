0f07fcabd15155880632bb3fdd3a741f4099e9a9
macm <mac.mitsch@comcast.net>
2022-07-20 23:12:23 -0500

    started seeds.sql table

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('Ron','Swanson',null,1);
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('Leslie','Knope',1,2);
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('','',null,3);
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('','',null,4);
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('','',null,5);
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('','',null,6);
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('','',null,7);
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('','',null,8);
+INSERT INTO employee(first_name, last_name,position_id, employee_id)
+VALUES ('','',null,9);
