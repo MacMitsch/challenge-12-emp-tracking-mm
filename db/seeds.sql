aa18c62dee30373779ca67e9458401b3eb624d63
macm <mac.mitsch@comcast.net>
2022-07-23 19:30:50 -0500

    creating seeds sql tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,24 @@
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
+INSERT INTO employee(first_name, last_name, position_id, employee_id)
 VALUES ('Ron','Swanson',null,1);
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
+INSERT INTO employee(first_name, last_name, position_id, employee_id)
 VALUES ('Leslie','Knope',1,2);
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
+INSERT INTO employee(first_name, last_name, position_id, employee_id)
 VALUES ('','',null,3);
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
+INSERT INTO employee(first_name, last_name, position_id, employee_id)
 VALUES ('','',null,4);
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
+INSERT INTO employee(first_name, last_name, position_id, employee_id)
 VALUES ('','',null,5);
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
+INSERT INTO employee(first_name, last_name, position_id, employee_id)
 VALUES ('','',null,6);
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
-VALUES ('','',null,7);
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
-VALUES ('','',null,8);
-INSERT INTO employee(first_name, last_name,position_id, employee_id)
-VALUES ('','',null,9);
+
+INSERT INTO position (title,department_id)
+VALUES('Director',1);
+INSERT INTO position (title,department_id)
+VALUES('Deputy Director',1);
+INSERT INTO position (title,department_id)
+VALUES('Director',1);
+
+INSERT INTO department (department_name)
+VALUES('Parks Department');
+INSERT INTO department (department_name)
+VALUES('Councilman');
